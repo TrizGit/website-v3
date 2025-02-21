a9cad1018bf82a3fa538a739ae24680f86f8a583
BuildTools <unconfigured@null.spigotmc.org>
2025-02-21 09:13:15 +0700

    migration v0.3.3 - 404 fix 2

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,5 +1,13 @@
 window.addEventListener('load', function() {
-    if (!document.querySelector('main')) {
-      window.location.href = '/website-v3/404.html';
-    }
+    fetch(window.location.href)
+    .then(response => {
+        if (!response.ok) {
+            // Redirect to custom 404 page if the response is not ok (404 or other errors)
+            window.location.href = '/website-v3/404.html';
+        }
+    })
+    .catch(() => {
+        // In case of a network error or other issues, also redirect to 404
+        window.location.href = '/website-v3/404.html';
+    });
 });
