e2165b8fb080b339206f15988797aae3935c4048
BuildTools <unconfigured@null.spigotmc.org>
2025-04-22 12:54:55 +0700

    v3.1.0 | Quick Fix 3

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-    document.getElementById('webVer').innerHTML = "b v3.0.10"; // Change the version
+    document.getElementById('webVer').innerHTML = "v3.1.0"; // Change the version
 };
