fbab6ad7fd4d9ef9184ad32bd6beb1928cac965d
BuildTools <unconfigured@null.spigotmc.org>
2025-06-02 21:28:25 +0700

    v3.2.0

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-    document.getElementById('webVer').innerHTML = "v3.1.3"; // Change the version
+    document.getElementById('webVer').innerHTML = "v3.2.0"; // Change the version
 };
