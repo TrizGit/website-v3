4f7d9ce6d210dcb8ace3aa8d4a8c99772d95b773
TrizGit <tristan.egbert1234@gmail.com>
2025-08-11 08:11:29 +0700

    v3.2.2 | Quick Fix 1

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-    document.getElementById('webVer').innerHTML = "v3.2.1"; // Change the version
+    document.getElementById('webVer').innerHTML = "v3.2.2"; // Change the version
 };
